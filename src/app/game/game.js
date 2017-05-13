d4c6e7a4bd7d2aef07dcdbfc1142f087955085e3
Edward Nicholes Jr <GameDevFox@gmail.com>
2017-05-12 20:29:47 -0500

    Move star with WASD controls

--- src/app/game/game.js
+++ src/app/game/game.js
@@ -21,6 +21,19 @@ export function unload(parent) {
   gameCanvas.appendTo('#hidden-game');
 }
 
+function getVector(wasd) {
+  let yPos = wasd.up.isDown ? -1 : 0;
+  yPos += wasd.down.isDown ? 1 : 0;
+
+  let xPos = wasd.left.isDown ? -1 : 0;
+  xPos += wasd.right.isDown ? 1 : 0;
+
+  return new Phaser.Point(xPos, yPos);
+}
+
+let star;
+let wasd;
+
 export function createGame(parent) {
   return new Phaser.Game(800, 450, Phaser.AUTO, parent, {
     preload: game => {
@@ -49,8 +62,22 @@ export function createGame(parent) {
       });
 
       // Add star
-      const star = game.add.sprite(stage.width / 2, stage.height / 2, 'star');
+      star = game.add.sprite(stage.width / 2, stage.height / 2, 'star');
       star.anchor.set(0.5);
+
+      // WASD Controller
+      wasd = input.keyboard.createCursorKeys();
+    },
+
+    update: game => {
+      const {stage} = game;
+
+      const vector = getVector(wasd);
+      vector.setMagnitude(200);
+
+      const pos = new Phaser.Point(stage.width / 2, stage.height / 2);
+      Phaser.Point.add(pos, vector, pos);
+      star.position.copyFrom(pos);
     },
 
     render: game => {
