fc65ec1d468c1266784cc12b0601a085917c6b0b
Edward Nicholes Jr <GameDevFox@gmail.com>
2017-08-26 20:36:51 -0500

    Use bootstrap-social.css insyead of *.scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import router from './app/factory';
 
 // CSS
-import 'bootstrap-social/bootstrap-social.scss';
+import 'bootstrap-social/bootstrap-social.css';
 import './index.scss';
 
 ReactDOM.render(router, document.getElementById('root'));
